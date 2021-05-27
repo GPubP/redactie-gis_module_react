081a5377219975fa664219d150a67e9c06c0162b
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2021-05-27 13:21:40 +0200

    fix: remove log

--- public/index.tsx
+++ public/index.tsx
@@ -1,8 +1,6 @@
 import { GisAddress, GisAddressView, GisReference } from './lib/components';
 import { formRendererConnector } from './lib/connectors';
 
-console.log('init');
-
 formRendererConnector.api.fieldRegistry.add([
 	{
 		name: 'gisReference',
