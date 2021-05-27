8d0537965a4ba36eabf50fb729d7d68b38e09a5f
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2021-05-27 09:47:51 +0200

    feat(location): remove search

--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,8 @@
 import { GisAddress, GisAddressView, GisReference } from './lib/components';
 import { formRendererConnector } from './lib/connectors';
 
+console.log('init');
+
 formRendererConnector.api.fieldRegistry.add([
 	{
 		name: 'gisReference',
